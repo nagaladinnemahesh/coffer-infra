2b900c0f7fc4bd63447ab09f56e38f7d8f41d6eb
nagaladinnemahesh <maheshnagaladinne@gmail.com>
2026-08-29 22:31:39 +0530

    fix: allow gke nodes to pull images

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -14,3 +14,9 @@ resource "google_project_iam_member" "gke_nodes_metric_writer" {
   role    = "roles/monitoring.metricWriter"
   member  = "serviceAccount:${google_service_account.gke_nodes.email}"
 }
+
+resource "google_project_iam_member" "gke_nodes_artifact_registry_reader" {
+  project = "coffer-506809"
+  role    = "roles/artifactregistry.reader"
+  member  = "serviceAccount:coffer-gke-nodes@coffer-506809.iam.gserviceaccount.com"
+}
